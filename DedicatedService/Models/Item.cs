ca4162836718613f9dcc32de541b82e2e70c01ea
Arif-Home <Arif-Home@DESKTOP-8U8GGL9>
2025-06-08 23:50:55 +0600

    Add project files.

---
+++ DedicatedService/Models/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DedicatedService.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+    }
+}

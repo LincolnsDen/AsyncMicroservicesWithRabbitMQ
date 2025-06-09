c21d77e18176233c313dddfe2638a015578e7147
Arif-Home <Arif-Home@DESKTOP-8U8GGL9>
2025-06-09 23:34:25 +0600

    Revert "Migration Steps 2"
    This reverts commit 93fa71e0793723099d746c80ec815cd3558319ea.

---
+++ DedicatedService/Models/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DedicatedService.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int ItemId { get; set; }
+        public int Quantity { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
---
+++ OrderApi/Data/OrderDbContext.cs
@@ -0,0 +1,14 @@
+﻿using DedicatedService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace OrderApi.Data
+{
+    public class OrderDbContext : DbContext
+    {
+        public OrderDbContext(DbContextOptions<OrderDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Item> Items { get; set; }
+    }
+}

c6e1cf6cf344b1a5f2a60fd7f622f6de76c91936
Arif-Home <Arif-Home@DESKTOP-8U8GGL9>
2025-06-09 22:22:56 +0600

    Migration step commit.

---
+++ DedicatedService/Models/Order.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace DedicatedService.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int ItemId { get; set; }
+        public int Quantity { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
---
+++ OrderApi/Data/OrderDbContext.cs
@@ -0,0 +1,13 @@
+﻿using DedicatedService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace OrderApi.Data
+{
+    public class OrderDbContext : DbContext
+    {
+        public OrderDbContext(DbContextOptions<OrderDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Order> Orders { get; set; }
+    }
+}

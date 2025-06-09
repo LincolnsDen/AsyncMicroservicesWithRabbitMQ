93fa71e0793723099d746c80ec815cd3558319ea
Arif-Home <Arif-Home@DESKTOP-8U8GGL9>
2025-06-09 23:29:41 +0600

    Migration Steps 2

--- OrderApi/Program.cs
+++ OrderApi/Program.cs
@@ -1,6 +1,3 @@
-using Microsoft.EntityFrameworkCore;
-using OrderApi.Data;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,7 +6,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<OrderDbContext>(o=>o.UseSqlite(builder.Configuration.GetConnectionString("Default")));
 
 var app = builder.Build();
 
